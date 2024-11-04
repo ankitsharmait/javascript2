d947fc54b04f11bd3593d62f70476cd261ebfd19
Ankit <ankitsharmabela@gmail.com>
2024-11-04 19:59:44 +0530

    some function of array is js

--- intro3.js
+++ intro3.js
@@ -1,1 +1,15 @@
 console.log("inside printing statement");
+let a=[1,2,3,4];
+console.log(a,a.length);
+for(let i =0;i<a.length;i++)
+{
+    console.log('index:',i, "value",a[i]);
+}
+a.push(5);//add value at the last
+console.log(a);
+a.pop(); // remove value from the last index
+console.log(a);
+a.unshift(10);//add value to the first index
+console.log(a);
+a.shift();//remove value from the first index
+console.log(a);
