ea33b832d3e0b8f1f6124759ba1f4a8bbe1b53da
Ankit <ankitsharmabela@gmail.com>
2024-11-08 12:42:11 +0530

    different types of functions

--- intro3.js
+++ intro3.js
@@ -1,4 +1,4 @@
-console.log("inside printing statement");
+/*console.log("inside printing statement");
 let a=[1,2,3,4];
 console.log(a,a.length);
 for(let i =0;i<a.length;i++)
@@ -12,4 +12,95 @@ console.log(a);
 a.unshift(10);//add value to the first index
 console.log(a);
 a.shift();//remove value from the first index
-console.log(a);
+console.log(a);*/
+/*function myFun(words)
+{
+ let temp = words.shift();
+ words.unshift("new");
+ words.unshift(temp);
+ words.splice(2,1);
+ let ans = words.join(",");
+ return ans;
+}
+let arr = ["apple","banana","cherry","mango"];
+let ans = myFun(arr);
+console.log(ans);*/
+/*
+let arr1=[1,2,3,4,5];
+let arr2 =[10,11,12,13];
+arr1 = arr2;
+console.log(arr1);
+
+
+const arr3=[1,2,3,4,5];
+const arr4 = [10,11,2,3];
+arr3 = arr4;
+console.log(arr3);*/
+/*function myFun()
+{
+    console.log("inside the function body");
+
+}
+ myFun();
+ console.log(myFun());*/
+/* const a = function()
+ {
+    console.log("hello  world");
+ }
+ const b = a;
+ a();
+ b();*/
+ /*function a(b)
+ {
+
+  b();
+ }
+ function c()
+ {
+    console.log("inside c function");
+ }
+ a(c);*/
+
+
+ /*const a = function()
+ {
+    console.log("inside function a");
+ }
+ a();*/
+/*
+ const a = ()=>
+ {
+    console.log("inside function");
+ }
+ a();
+*/
+ /*(function()
+{
+    console.log("inside iife function");
+})()
+*//*
+const myfun = ()=>
+{
+   console.log("inside Arrow function");
+}
+myfun();
+*/
+
+/*console.log("before");
+setTimeout(fn,5000);
+function fn()
+{
+   console.log("i am in fn");
+}
+*/
+/*const fun = ()=>
+{
+   console.log("hello world");
+}
+fun();*/
+myfun();
+
+function myfun()
+{
+   console.log("hello world");
+}
